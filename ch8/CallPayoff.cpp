d309a5eb2ee5d49846a7d3f5533101acec874382
Hai Tang <402696415@qq.com>
2016-02-21 22:18:51 -0600

    Feb 21st

---
+++ ch8/CallPayoff.cpp
@@ -0,0 +1,49 @@
+// CallPayoff.cpp
+//
+// CallPayoff class.
+//
+// (C) Shujie CHEN @2016
+//
+
+#include "CallPayoff.hpp"
+
+using namespace std;
+
+// Constructors and destructors
+CallPayoff::CallPayoff():Payoff()
+{	// Default constructor
+
+	K = 100.0;	// Prototype value
+}
+
+CallPayoff::CallPayoff(double strike):Payoff()
+{	// Normal constructor
+
+	K = strike;
+}
+
+CallPayoff::CallPayoff(const CallPayoff& source):Payoff(source)
+{	// Copy constructor
+
+	K = source.K;
+}
+
+CallPayoff::~CallPayoff()
+{
+	cout << "Shit UT Dallas" << endl;
+}
+
+double CallPayoff::Strike() const
+{	//
+	return K;
+}
+
+void CallPayoff::Strike(double NewStrike)
+{	// Set K
+	K = NewStrike;
+}
+
+CallPayoff& CallPayoff::operator = (const CallPayoff &source)
+{	// Assignment operator
+
+}
