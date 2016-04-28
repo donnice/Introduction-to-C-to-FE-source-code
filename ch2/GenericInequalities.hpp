4caedbb20d31b8308c7cedf1558fc56438eb2e1b
Hai Tang <402696415@qq.com>
2016-04-28 15:15:49 -0700

    Apr 28th

---
+++ ch2/GenericInequalities.hpp
@@ -0,0 +1,15 @@
+//GenericInequalities.hpp
+//
+//Header file containing declarations of functions
+//
+//This is the template/generic version
+//
+//(C) Shujie CHEN 2016
+//
+
+#ifdef GenericInequalities_HPP
+#define GenericInequalities_HPP
+
+/////////////////Useful functions///////////////////
+
+//Max and Min of two numbers
