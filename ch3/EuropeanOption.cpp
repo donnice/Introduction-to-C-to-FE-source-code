db9912c5ec80bc7de1e7a6bb9193a7b21711761d
Hai Tang <402696415@qq.com>
2016-05-05 12:54:05 -0700

    May 5th

--- ch3/EuropeanOption.cpp
+++ ch3/EuropeanOption.cpp
@@ -2,6 +2,8 @@
 //
 // Author: Daniel Duffy
 //
+// Black–Scholes model
+//
 // (C) Shujie CHEN @2016
 //	
 
@@ -10,9 +12,29 @@
 #include <math.h>				// For mathematical functions, exp()
 
 // Kernel Functions
+double N(double x) const
+{
+	if(x < 0)
+		return 0;
+	else if(x > 0 && x < 1)
+		return x;
+	else
+		return 1;
+}
+
 double EuropeanOption::CallPrice() const
 {
 	double tmp = sig * sqrt(T);
 
-	double d1 = (log(U/K))
+	double d1 = (log(U/K)+(b+(sig*sig)*0.5)*T)/tmp;
+	double d2 = d1 - tmp;
+
+	return (U * exp((b-r)*T)*N(d1)) - (K*exp(-r*T)*N(d2)); // N is the cumulative distribution function of standard normal distribution
+
 }
+
+double EuropeanOption::PutPrice() const
+{
+	
+}
+
