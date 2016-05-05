effc9fbd6a29a650cc31249b2fb5b53c066b16b5
Hai Tang <402696415@qq.com>
2016-05-05 11:43:13 -0700

    May 5th

---
+++ ch3/EuropeanOption.hpp
@@ -0,0 +1,40 @@
+class EuropeanOption
+{
+private:
+
+	void init();		// Initialise all default values
+	void copy(const EuropeanOption& o2);
+	//"Kernel" functions for option calculations
+	double CallPrice() const;
+	double PutPrice() const;
+	double CallDelta() const;
+	double PutDelta() const;
+
+public:
+	// Public member data for convenience
+	double r;			// Interest rate
+	double sig;			// Volatility
+	double K;			// Strike price
+	double T;			// Expiry date
+	double U;			// Current underlying price
+	double b;			// Cost of carry
+
+public:
+	// Constructors
+	EuropeanOption(); // Default call option
+	EuropeanOption(const EuropeanOption& option2); // Copy constructor
+	EuropeanOption(const string& optionType); // Create option type
+
+	// Destructor
+	virtual ~EuropeanOption();
+
+	// Assignment operator
+	EuropeanOption& operator = (const EuropeanOption& option2);
+
+	// Functions that calculate option price and sensitivities
+	double Price() const;
+	double Delta() const;
+
+	// Modifier functions
+	void toggle();
+}
