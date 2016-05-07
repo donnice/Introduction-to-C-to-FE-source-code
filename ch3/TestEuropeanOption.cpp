64bf0f363991ec07aebff67cba99bbef2809e512
Hai Tang <402696415@qq.com>
2016-05-07 11:45:22 -0700

    I am back

--- ch3/TestEuropeanOption.cpp
+++ ch3/TestEuropeanOption.cpp
@@ -24,5 +24,31 @@ int main()
 	indexOption.optType = "P";
 	indexOption.U = 100.0;
 	indexOption.K = 95.0;
+	indexOption.T = 0.5;
+	indexOption.r = 0.10;
+	indexOption.sig = 0.2;
+	double q = 0.05;
+	indexOption.b = indexOption.r - q;
+
+	cout << "Put option on index:"<< indexOption.Price()<<endl;
+
+	// Call and put options on a future
+	EuropeanOption futureOption;
+	futureOption.optType = "P";
+	futureOption.U = 19.0;
+	futureOption.K = 19.0;
+	futureOption.T = 0.75;
+	futureOption.r = 0.10;
+	futureOption.sig = 0.28;
+	futureOption.b = 0.0;
+
+	cout << "Put option on future: " << futureOption.Price() << endl;
+
+	// Now change over to call on the option
+	futureOption.toggle();
+	cout << "Call on future: " << futureOption.Price() << endl;
+
+	return 0;
+	
 
 }
