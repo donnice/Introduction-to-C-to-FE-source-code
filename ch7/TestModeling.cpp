726017d973549897c4550b944b7f14183cd749e7
Hai Tang <402696415@qq.com>
2016-05-16 15:57:01 +0800

    May 16th

--- ch7/TestModeling.cpp
+++ ch7/TestModeling.cpp
@@ -12,5 +12,61 @@ using namespace std;
 void genericFunction(double myX, double myY,
 						double (*f)(double x, double y))
 {
-	// Call function
+	// Call function f with arguments myX and myY
+	// Passed to the pointer function f
+	// f represents pointer function
+	double result = (*f)(myX, myY);	
+
+	cout << "Result is: " << result << endl;
+}
+
+double add(double x, double y)
+{
+	cout << "** Adding two numbers: " << x << "," << y << endl;
+	return x+y;
+}
+
+double multiply(double x, double y)
+{
+	cout << "** Multiplying two numbers:" << x << ", " << y << endl;
+	return x*y;
+}
+
+double substract(double x, double y)
+{
+	cout << "** Substracting two numbers:" << x << ", " << y << endl;
+	return x - y;	
+}
+
+namespace StandardInterface
+{
+	// Namespace consisting of function pointers
+	double (*func1)(double x);
+	double (*func2)(double x, double y);
+}
+
+namespace Implementation1
+{
+	double F1(double x) { return x; }
+	double F2(double x, double y){ return x*y; }
+}
+
+namespace Implementation2
+{
+	double G1(double x){ return -x; }
+	double G2(double x, double y){ return -x*y; }
+}
+
+int main()
+{
+	double x = 3.0;
+	double y = 2.0;
+	genericFunction(x,y,add);
+	genericFunction(x,y,multiply);
+	genericFunction(x,y,substract);
+
+	StandardInterface::func1 = Implementation1::F1;
+	StandardInterface::func2 = Implementation1::F2;
+
+	return 0;
 }
