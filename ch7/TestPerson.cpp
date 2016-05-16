8802568682b0a197e77b67a2a6d4fb9f144b0ea7
Hai Tang <402696415@qq.com>
2016-05-16 17:03:33 +0800

    May 16th

--- ch7/TestPerson.cpp
+++ ch7/TestPerson.cpp
@@ -12,10 +12,56 @@
 
 using namespace std;
 
+class Employee: public Person
+{
+public:
+	string fun;	// Function
+	double sal;	// Salary
+	int wAge;	// Working age
+
+	Employee();
+	Employee(const string& name, const int& age, const string& function,
+		const double& salary, const int& workingAge);
+	virtual ~Employee();
+	void print() const;	// Print belongs to Employee
+};
+
+Employee::Employee()
+{
+
+}
+
+Employee::Employee(const string& name, const int& age, const string& function,
+		const double& salary, const int& workingAge)
+{
+	Person::Person(name, age);
+	fun = function;
+	sal = salary;
+	wAge = workingAge;
+}
+
+Employee::~Employee()
+{
+	cout << "Warning: Employee is destructed!" << endl;
+}
+
+void Employee::print() const
+{
+	cout << "\n **Employee Data** \n";
+	Person::print();
+	cout << "\nFunction: " << fun << ", Salary:" << sal
+		<< ", has already worked for: " << wAge << " year(s)." << endl; 
+}
+
 int main()
 {
 	Person dd("Shujie CHEN",24);
 	dd.print();
+
+	cout << "===========================" << endl;
+
+	Employee dd2("Shujie", 24, "Web Developer", 58000.0, 1);
+	dd2.print();
 
 	return 0;
 }
