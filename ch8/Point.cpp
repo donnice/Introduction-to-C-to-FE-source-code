23777e62f565ab3a0b4e395cc0b5ffc8ee372100
Hai Tang <402696415@qq.com>
2016-06-19 23:34:16 -0500

    Jun 19th

--- ch8/Point.cpp
+++ ch8/Point.cpp
@@ -7,6 +7,8 @@
 
 #include "Point.hpp"
 
+using namespace std;
+
 // Private functions
 void Point::init(double dx, double dy)
 {
@@ -22,5 +24,37 @@ Point::Point()
 Point::Point(double newx, double newy)
 { // Initialize using newx and newy
 	init(newx, newy);
+}
+
+Point::Point(const Point& pt)
+{ // Copy constructor
+	x = pt.x;
+	y = pt.y;
+}
+
+Point::~Point()
+{
+	cout << "I am destructed!" << endl;
+}
+
+double Point::getX() const
+{
+	return x;
+}
+
+double Point::getY() const
+{
+	return y;
+}
+
+// Arithmetic functions
+Point Point::add(const Point& p) const
+{
+	return Point(x + p.x, y + p.y);
+}
+
+Point Point::substract(const Point& p) const
+{
+	return Point(x - p.x, y - p.y);
 }
 
