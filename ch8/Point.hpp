9c6457bf5fdb07d9bb177eb1b65e97aa7d39c938
Hai Tang <402696415@qq.com>
2016-06-19 20:41:15 -0500

    Jun 19th

--- ch8/Point.hpp
+++ ch8/Point.hpp
@@ -25,8 +25,24 @@ private:
 
 public:
 	// Constructors
-	Point();							// Default constructor
-	Point(double xval, double yval);	// Initialize with value
-	Point(const Point& pt);				// Copy constructor
-	
-}
+	Point();								// Default constructor
+	Point(double xval, double yval);		// Initialize with value
+	Point(const Point& pt);					// Copy constructor
+	~Point();								// Destructor
+
+	// Accessing functions
+	double getX() const;					// The x-coordinate
+	double getY() const;					// The y-coordinate
+
+	// Arithmetic functions
+	Point add(const Point& p) const;		// Return current + p;
+	Point substract(const Point& p) const; 	// Return current - p;
+	Point scale(const Point& p) const;		// Return current * p;
+
+	// Copy
+	Point& copy(const Point& p);			// Copy p in current
+};
+
+	ostream& operator << (ostream& os, const Point& p);
+
+#endif
