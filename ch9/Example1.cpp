102720b67ff1ea8998ae1b458434600ed9d8e9c2
Hai Tang <402696415@qq.com>
2016-06-26 20:41:27 -0500

    Jun 26th

--- ch9/Example1.cpp
+++ ch9/Example1.cpp
@@ -16,4 +16,70 @@ class Base
 public:
 	Base() {}
 	virtual ~Base(){}
+
+	virtual void print() const
+	{
+		cout << "I'm base!\n";
+	}
+};
+
+class D1:public Base
+{
+
+public:
+	D1(){}
+	virtual ~D1(){}
+
+	virtual void print() const
+	{
+		cout << "I'm a D1!\n";
+	}
+
+	virtual double calculate(double d) const
+	{
+		return 1.0*d;
+	}
+};
+
+class D2: public Base
+{
+public:
+	D2(){}
+	virtual ~D2(){}
+
+	virtual void print() const
+	{
+		cout << "I'm D2!\n";
+	}
+
+	virtual double calculate(double d) const
+	{
+		return 2.0*d;
+	}
+
+};
+
+int main()
+{
+
+	// part I: typeinfo
+
+	D1 d1, d11;
+	D2 d2;
+
+	// Define a reference to type
+	const type_info& myRef = typeid(d1);
+
+	cout << "Human readable name:" << myRef.name() << endl;
+
+	// Test if two objects are of same type or not
+	if(typeid(d1) == typeid(d11))
+	{
+		cout << "Typees are the same!" << endl;
+	}
+
+	
+
+
+	return 0;
 }
